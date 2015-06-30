86de4c91c68215c3bd754c0db43ebf56a38cad23
Andrew Jones <aj@bignerdranch.com>
2015-06-30 15:08:08 -0400

    Initial css-based output

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
+var spritesmith = require('gulp.spritesmith');
 
 gulp.task('default', function() {
     // place code for your default task here
+});
+
+gulp.task('sprite', function () {
+    var spriteData = gulp.src('img/sprites/*.png')
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: 'sprite.css'
+        }));
+    spriteData.img.pipe(gulp.dest('img'));
+    spriteData.css.pipe(gulp.dest('css'));
 });
