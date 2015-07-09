dc748c52ee04d10ebc734426eb302ee031277f60
Andrew Jones <aj@bignerdranch.com>
2015-07-09 16:37:40 -0400

    added updates for sass templates, etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp        = require('gulp');
+var sass        = require('gulp-sass');
 var watch       = require('gulp-watch');
 var spritesmith = require('gulp.spritesmith');
 
@@ -7,18 +8,26 @@ gulp.task('default', ['watch']);
 
 
 gulp.task('sprite', function () {
-    var spriteData = gulp.src('img/sprites/*.png')
+    var spriteData = gulp.src('img/sprites/*')
         .pipe(spritesmith({
             /* this whole image path is used within the css background declaration */
             imgName: '../img/sprite.png',
-            cssName: 'sprite.css'
+            cssName: 'sprite.scss',
+            cssTemplate: 'css/scss.template.handlebars'
         }));
     spriteData.img.pipe(gulp.dest('img'));
     spriteData.css.pipe(gulp.dest('css'));
 });
 
 
+gulp.task('sass', function() {
+    return gulp.src('css/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
+});
+
+
 // Watch for changes to sprite images
 gulp.task('watch', function () {
-    gulp.watch(['img/sprites/**/*.png'], ['sprite']);
+    gulp.watch(['img/sprites/**/*'], ['sprite']);
 });
